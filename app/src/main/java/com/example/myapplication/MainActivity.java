7520e355b7c9f35e8b5eaafd748111604b87ff28
nikysdoughter <62029158+nikysdoughter@users.noreply.github.com>
2020-03-10 20:28:55 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recipes = findViewById(R.id.Recipe);
+        // comment
     }
 }
