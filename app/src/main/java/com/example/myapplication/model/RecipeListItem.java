87c5cc7da749bfe21d486c8c6c7f7801efd16cf3
za-ek <tz@za-ek.ru>
2020-03-12 22:49:45 +0300

    Модель элемента списка

---
+++ app/src/main/java/com/example/myapplication/model/RecipeListItem.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.model;
+
+import java.util.ArrayList;
+
+public class RecipeListItem {
+    private String mName;
+    private boolean mOnline;
+
+    public RecipeListItem(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    private static int lastRecipeId = 0;
+
+    public static ArrayList<RecipeListItem> createContactsList(int numContacts) {
+        ArrayList<RecipeListItem> recipes = new ArrayList<RecipeListItem>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            recipes.add(new RecipeListItem("Recipe " + ++lastRecipeId));
+        }
+
+        return recipes;
+    }
+}
