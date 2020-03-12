cb271cfcb76c90987fb30c5e70011f17e656571f
za-ek <tz@za-ek.ru>
2020-03-12 23:05:33 +0300

    Подключаю список в приложение

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.myapplication.model.RecipeListItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView mRecentRecyclerView = findViewById(R.id.RecipeList);
+
+        ArrayList<RecipeListItem> recipes = RecipeListItem.createRecipeList(30);
+        MainActivityAdapter adapter = new MainActivityAdapter(recipes);
+        mRecentRecyclerView.setAdapter(adapter);
+        mRecentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         // comment
     }
 }
